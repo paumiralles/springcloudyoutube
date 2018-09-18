c5d598e974dd454e4f99c2ec89dd324f445fa8f5
Pau Miralles Rovira <pmirallr@everis.com>
2018-09-18 12:08:46 +0200

    update6

--- src/main/java/com/springcloudyoutube/configserver/ConfigServerApplication.java
+++ src/main/java/com/springcloudyoutube/configserver/ConfigServerApplication.java
@@ -2,11 +2,12 @@ package com.springcloudyoutube.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigServerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
